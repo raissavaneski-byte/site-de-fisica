b09c8ab91434ce7505abbef339935dcbcec43546
raissavaneski-byte <raissa.vaneski@escola.pr.gov.br>
2026-06-26 10:32:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Theme Toggle (opcional)
+const themeToggle = document.getElementById('theme-toggle');
+if (themeToggle) {
+    themeToggle.addEventListener('click', () => {
+        document.body.classList.toggle('dark');
+    });
+}
